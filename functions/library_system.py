5e0edc968c3a375e16abe08df48ad5039ad6ac1e
sibuthejhb124 <sibuthejhb124@student.wethinkcode.co.za>
2024-11-25 12:53:17 +0200

    test done I think?

--- functions/library_system.py
+++ functions/library_system.py
@@ -1,12 +1,13 @@
 DATABASE_FILE = "./database/books.txt"
+# DATABASE_FILE = "demo.txt"
 
 def initialize_database():
     """
     Initialize the database file if it doesn't exist.
     """
     with open(DATABASE_FILE, 'a') as db:
-        pass  # Ensure the file exists
-
+        db.write('')
+        
 def add_book(title, author):
     """
     Add a new book to the library.
@@ -14,6 +15,8 @@ def add_book(title, author):
     :param author: The author of the book
     """
     # TODO: Append the book's title and author to the database file
+    db = open(DATABASE_FILE , 'w')
+    db.write(f'{title},{author}')
 
 def search_book(title):
     """
@@ -22,6 +25,16 @@ def search_book(title):
     :return: A dictionary with the book's details if found, else None
     """
     # TODO: Implement logic to search for a book in the database file
+    with open(DATABASE_FILE , 'r') as db:
+        db = db.readlines()
+        for book in db:
+            book = book.replace('\n' , '')
+            book = book.split(',')
+            if title == book[0]:
+                return {"title":book[0] , "author":book[1]}
+        # db = list(db)
+        
+
 
 def list_books():
     """
@@ -29,3 +42,17 @@ def list_books():
     :return: A list of dictionaries with each book's details
     """
     # TODO: Read all books from the database file and return them as a list of dictionaries
+    with open(DATABASE_FILE , 'r') as db:
+        db = db.readlines()
+        books = []
+        for index , book in enumerate(db):
+            if index == 0:
+                continue
+            book = book.replace('\n' , '')
+            book = book.split(',')
+            books.append({"title":book[0] , "author":book[1]})
+            
+        return books
+        
+
+# print(search_book("1984"))
